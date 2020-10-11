691815703ee10e41021b8706aefb7788a29b3630
pablolopez02 <72487652+pablolopez02@users.noreply.github.com>
2020-10-11 09:24:05 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,3 +1,6 @@
-# Define aquí mismo la función my_email sin argumentos que 
-# devuelve tu correo electrónico de la UCLM
+def my_email;
+    c = pablo.lopez35@alu.uclm.es
+  
+my_email
+my_email (c)
 
